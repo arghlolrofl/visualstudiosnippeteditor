3fead047f88f2a49ec25eef685848c7a5bee9008
me <me@komsa.extern.de>
2015-11-26 15:33:55 +0100

    Refactoring ...

---
+++ VisualStudioSnippetEditor/ViewModel/EditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using VisualStudioSnippetEditor.Contracts;
+
+namespace VisualStudioSnippetEditor.ViewModel
+{
+  public class EditViewModel : ViewModelBase
+  {
+    private ISnippet _snippet;
+    public ISnippet Snippet
+    {
+      get { return _snippet; }
+      set { _snippet = value; RaisePropertyChanged(); }
+    }
+
+
+    public void Initialize(ISnippet snippet)
+    {
+      Snippet = snippet;
+    }
+  }
+}
